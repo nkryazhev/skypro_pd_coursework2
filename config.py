38dcdfad9085ca6c57f4934cc49936198ab3f3bf
nkryazhev <nkryazhev@gmail.com>
2022-03-30 16:25:57 +0300

    update config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+POSTS_FILE = 'data.json'
+COMMENTS_FILE = 'comments.json'
+BOOKMARKS_FILE = 'bookmarks.json'
+PREVIEW_LIM = 50
+SEARCH_LIM = 10
